377b7d003fb6b3f2556d1b28939ece194708562d
sijhua <81196825+sijhua@users.noreply.github.com>
2021-04-03 22:29:16 +1100

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 
 data = pd.read_csv("train.csv", header = None)
 data = data.replace(9999, np.nan)
@@ -16,3 +17,36 @@ for label in prior_dict.keys():
     std_array.append(categorised_data.std(axis = 0, skipna = True).values)
 mean_array = np.array(mean_array)
 std_array = np.array(std_array)
+
+def calculateProbability(x, mean, std):
+    exponent = math.exp(-(math.pow(x-mean, 2)/(2*math.pow(std, 2))))
+    return (1/(math.sqrt(2*math.pi)*std))*exponent
+
+def calculateClassProbabilities(testVector):
+    column_number = len(mean_array[0])
+    probabilities = {}
+    key_num = 0
+    for label in prior_dict.keys():
+        probabilities[label] = 1
+        for i in range(column_number):
+            x = testVector[i]
+            probabilities[label] *= calculateProbability(x, mean_array[key_num][i], std_array[key_num][i])
+        key_num+=1;
+    return probabilities
+
+list = test.values.tolist()
+predictions = []
+for i in range(0,len(test)):
+    probabilities = calculateClassProbabilities(list[i][1:])
+    bestLabel, bestProb = None, -1
+    for label, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = label
+    predictions.append(bestLabel)
+
+correct = 0
+for i in range(len(test)):
+    if list[i][0] == predictions[i]:
+        correct += 1
+print(correct/float(len(test)))
